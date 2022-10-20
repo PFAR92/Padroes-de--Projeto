86e01043f4acfd22cdc50959b1c5f1f431166e84
PFAR92 <paulofelipetj@hotmail.com>
2022-10-20 13:27:38 -0300

    20/10/22 13:27

--- lab-padroes-projeto-java/src/strategy/ComportamentoNormal.java
+++ lab-padroes-projeto-java/src/strategy/ComportamentoNormal.java
@@ -4,7 +4,7 @@ public class ComportamentoNormal implements Comportamento{
 
     @Override
     public void mover() {
-        System.out.println("Movendo-se normalmente");
+        System.out.println("Movendo-se normalmente...");
         
     }
     
